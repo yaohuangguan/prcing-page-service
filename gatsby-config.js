09b98745dc417b9beb3975273397e4f6ce0b5196
yaohuangguan <yaob@miamioh.edu>
2020-05-01 11:27:48 +0800

    Initial commit from gatsby: (https://github.com/W3Layouts/gatsby-starter-delog.git)

---
+++ gatsby-config.js
@@ -0,0 +1,72 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `Delog`,
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://delog-w3layouts.netlify.com/`,
+    home: {
+      title: `Hi! I'm Delog`,
+      description: `I have been specifically designed to become a digital home for designers and developers, help them build amazing professional looking websites with ease. You don't have to worry about nitty gritty of web hosting services to run a blog and yet take full advantage of CMS to manage content :)`,
+    },
+    /* W3Layouts domain verification key for contact forms https://my.w3layouts.com/Forms/ */
+    w3l_dom_key: `5e609f7a2d23fCF_Domain_verify` 
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/_data`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            classPrefix: "language-",
+            inlineCodeMarker: null,
+            aliases: {},
+            showLineNumbers: false,
+            noInlineHighlight: false,
+          },
+        },
+        {
+          resolve: 'gatsby-remark-emojis',
+        }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it. replace with yours
+        trackingId: "UA-164743872-1",
+        head: true,
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Delog GatbsyJS Starter`,
+        short_name: `Delog`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#381696`,
+        display: `standalone`,
+        icon: "src/images/icon.png",
+      },
+    },
+    `gatsby-plugin-sass`, 
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
+    'gatsby-plugin-dark-mode',
+    `gatsby-plugin-offline`,
+  ],
+}
